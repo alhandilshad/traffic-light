0fa2d9aac39f2aade8408fa4b12dc2aed07122e1
alhandilshad <dilshadalhan@gmail.com>
2024-09-20 19:51:16 +0500

    traffic signal complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
-import ReactPlayer from 'react-player'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
+  const [red, setred] = useState(true);
+  const [yellow, setyellow] = useState(false);
+  const [green, setgreen] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (red) {
+        setred(false);
+        setyellow(true);
+        setgreen(false);
+      } else if (yellow) {
+        setred(false);
+        setyellow(false);
+        setgreen(true);
+      } else if (green) {
+        setred(true);
+        setyellow(false);
+        setgreen(false);
+      }
+    }, 2000);
+
+    // Clean up the interval on component unmount
+    return () => clearInterval(interval);
+  }, [red, yellow, green]);
+
   return (
     <>
-    <div>App</div>
-    <video src='https://www.shutterstock.com/shutterstock/videos/1098687287/preview/stock-footage-animated-video-of-old-racing-car-game-in-bit-style-with-other-cars-competing-arcade-pixel-d.webm'></video>
+    <div className='flex justify-center items-center w-full h-[100vh] bg-gray-200'>
+      <div className='bg-black w-20 h-52 flex flex-col justify-center items-center gap-2'>
+        <div className={`${red ? 'bg-red-600' : 'bg-white'} w-14 h-14 rounded-full`}></div>
+        <div className={`bg-white ${yellow ? 'bg-yellow-600' : 'bg-white'} w-14 h-14 rounded-full`}></div>
+        <div className={`bg-white ${green ? 'bg-green-600' : 'bg-white'} w-14 h-14 rounded-full`}></div>
+      </div>
+    </div>
     </>
   )
 }
