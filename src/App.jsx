c768205d505b00aef56d207e680defa3c2117100
alhandilshad <dilshadalhan@gmail.com>
2024-09-20 15:04:13 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactPlayer from 'react-player'
+
+const App = () => {
+  return (
+    <>
+    <div>App</div>
+    <video src='https://www.shutterstock.com/shutterstock/videos/1098687287/preview/stock-footage-animated-video-of-old-racing-car-game-in-bit-style-with-other-cars-competing-arcade-pixel-d.webm'></video>
+    </>
+  )
+}
+
+export default App;
